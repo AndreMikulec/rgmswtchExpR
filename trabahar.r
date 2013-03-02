a76fd53ee1da874e12761a255858fba55b75098d
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2013-03-01 22:06:28 -0600

    Basics

--- trabahar.r
+++ trabahar.r
@@ -3,5 +3,28 @@
 # Author: Administrator
 ###############################################################################
 
-Some Chanage
+# some(more) test code...
+myFunction <- function(x) {
 
+	a <- 2
+	b <- 3
+	
+	c <- a+b+x
+	c
+}
+
+myFunction2 <- function() {
+	
+	x <-90
+	a <-20
+	myFunction(5)
+	x
+	a
+	
+}
+
+# library(debug)
+# setwd("M:\\YDrive\\All_Economics\\eclipse_workspace\\rgmswtchExpR")
+# source("M:/YDrive/All_Economics/eclipse_workspace/rgmswtchExpR/trabahar.r")
+# mtrace(myFunction)
+# myFunction(5)
